3c9666ae80863974c77706eff23fb09516b03d7e
Houssan Ali Hijazi <hussanhijazi@gmail.com>
2019-03-07 10:28:29 -0300

    Add datasource and use case DI

--- app/src/main/java/br/com/hussan/stonechallenge/di/AppModule.kt
+++ app/src/main/java/br/com/hussan/stonechallenge/di/AppModule.kt
@@ -5,6 +5,5 @@ import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 
 val appModule = module {
-    // MyViewModel ViewModel
     viewModel { FactsViewModel(get()) }
 }
--- data/src/main/java/br/com/hussan/stonechallenge/data/di/DataModule.kt
+++ data/src/main/java/br/com/hussan/stonechallenge/data/di/DataModule.kt
@@ -1,10 +1,12 @@
 package br.com.hussan.stonechallenge.data.di
 
+import br.com.hussan.stonechallenge.data.datasource.CategoryDatasource
+import br.com.hussan.stonechallenge.data.datasource.CategoryRepository
 import br.com.hussan.stonechallenge.data.datasource.FactDatasource
 import br.com.hussan.stonechallenge.data.datasource.FactRepository
 import org.koin.dsl.module.module
 
 val dataModule = module {
-    // single instance of HelloRepository
     single<FactDatasource> { FactRepository(get()) }
+    single<CategoryDatasource> { CategoryRepository(get()) }
 }
